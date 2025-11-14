d62c32113f43eb41368400756746eacefd2a2f1f
sourav kumar <126371866+SOURAVkrdhn25@users.noreply.github.com>
2025-11-14 18:22:57 +0530

    Add files via upload

---
+++ flightreservation/src/main/java/com/flight/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.flight.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String handle(){
+        return "Hello my dear";
+    }
+
+    
+}
